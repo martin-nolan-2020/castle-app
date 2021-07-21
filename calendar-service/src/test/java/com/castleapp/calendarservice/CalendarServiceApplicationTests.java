4199515c77e18d8e37d7b41b7b51d8fd46a0ffac
martinnolan <martinnolan73517@gmail.com>
2021-07-21 18:36:50 +0100

    created the calendar-service microservice

---
+++ calendar-service/src/test/java/com/castleapp/calendarservice/CalendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castleapp.calendarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
