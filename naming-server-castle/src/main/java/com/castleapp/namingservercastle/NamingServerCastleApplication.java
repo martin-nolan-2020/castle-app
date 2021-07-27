c7e3100350255ce084a374351dbf1236e7822929
martinnolan <martinnolan73517@gmail.com>
2021-07-27 13:27:19 +0100

    developing eureka naming server microservice

---
+++ naming-server-castle/src/main/java/com/castleapp/namingservercastle/NamingServerCastleApplication.java
@@ -0,0 +1,13 @@
+package com.castleapp.namingservercastle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerCastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerCastleApplication.class, args);
+	}
+
+}
