fdabe381255d19cea1eaf756a6f638341373d8f6
martinnolan <martinnolan73517@gmail.com>
2021-07-29 16:22:55 +0100

    some simple logging set up

---
+++ calendar-service/src/main/java/com/castleapp/calendarservice/controllers/CircuitBreakerController.java
@@ -0,0 +1,13 @@
+package com.castleapp.calendarservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+
+	@GetMapping("/sample-api")
+	public String sampleApi() {
+		return "Martin's Sample API";
+	}
+}
