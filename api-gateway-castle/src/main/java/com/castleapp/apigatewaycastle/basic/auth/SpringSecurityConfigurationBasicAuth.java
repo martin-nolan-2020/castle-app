e890356e0c5df3ae70e7b2f7df1a41ea77c798e7
martinnolan <martinnolan73517@gmail.com>
2021-08-16 17:00:33 +0100

    security changes

--- api-gateway-castle/src/main/java/com/castleapp/apigatewaycastle/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ api-gateway-castle/src/main/java/com/castleapp/apigatewaycastle/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,17 +1,59 @@
 package com.castleapp.apigatewaycastle.basic.auth;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		//this.logger.debug("Using default configure(HttpSecurity). "
+//		//		+ "If subclassed this will potentially override subclass configure(HttpSecurity).");
+//		
+////		http.csrf().disable();
+////		
+////		//http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+////		http.authorizeRequests()
+////			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+////				.anyRequest().authenticated();
+////			
+////		//http.formLogin();
+////		http.httpBasic();
+//		
+//		http
+//			.csrf().disable()
+//			.authorizeRequests()
+//			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//				.anyRequest().authenticated()
+//				.and().httpBasic();
+//	}
+//} 
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
-
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter{
+	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//this.logger.debug("Using default configure(HttpSecurity). "
-		//		+ "If subclassed this will potentially override subclass configure(HttpSecurity).");
-		
-		http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
-		http.formLogin();
-		http.httpBasic();
+		http
+		.csrf().disable()	
+		.authorizeRequests()
+		.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			//.formLogin().and()
+			.httpBasic();
 	}
-} 
-
+}
